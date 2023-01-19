fca5b918d266ca36211465569abfbc3065e9dd3f
ChiharuT06 <u.chiharu0909@gmail.com>
2023-01-19 23:42:43 +0900

    READ ME追加

---
+++ treasure.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2023-01-19 15:23:13
+-- サーバのバージョン： 10.4.27-MariaDB
+-- PHP のバージョン: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `treasure`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `user_table`
+--
+
+CREATE TABLE `user_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `lid` varchar(12) NOT NULL,
+  `lpw` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `user_table`
+--
+
+INSERT INTO `user_table` (`id`, `name`, `lid`, `lpw`) VALUES
+(1, 'テスト君', 'test1', 'test1');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `user_table`
+--
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `user_table`
+--
+ALTER TABLE `user_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
